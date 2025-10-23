930b12dee6eee8508d1ce61d20812dbcdfa2022a
Muhammad Jawad <112897469+muhammadJawadm@users.noreply.github.com>
2025-10-23 19:51:36 +0500

    final changes

--- .next/server/chunks/ssr/[root-of-the-server]__5e6e8bee._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__5e6e8bee._.js
@@ -617,18 +617,18 @@ const HeroBanner3 = ()=>{
                                                 }, this),
                                                 formErrors.phone ? /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("span", {
                                                     className: "jsx-c2b835511f3efb59" + " " + "error-message",
-                                                    children: formErrors.phone
+                                                    children: formErrors.phone.replace(/'/g, "&apos;")
                                                 }, void 0, false, {
                                                     fileName: "[project]/src/app/Components/HeroBanner/HeroBanner3.tsx",
                                                     lineNumber: 442,
-                                                    columnNumber: 5
+                                                    columnNumber: 41
                                                 }, this) : /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("span", {
                                                     className: "jsx-c2b835511f3efb59" + " " + "phone-format-help",
                                                     children: "UK format: 07XXX XXXXXX or +447XXX XXXXXX"
                                                 }, void 0, false, {
                                                     fileName: "[project]/src/app/Components/HeroBanner/HeroBanner3.tsx",
                                                     lineNumber: 444,
-                                                    columnNumber: 5
+                                                    columnNumber: 41
                                                 }, this)
                                             ]
                                         }, void 0, true, {
