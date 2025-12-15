da4f49bcf3b1870beec26635317c1d05339e2538
Muhammad Jawad <112897469+muhammadJawadm@users.noreply.github.com>
2025-12-15 17:56:00 +0500

    change the mail of forms

--- src/app/api/contact-mail/route.ts
+++ src/app/api/contact-mail/route.ts
@@ -28,7 +28,7 @@ export async function POST(request: Request) {
             );
         }
 
-        const toAddress =  'mrtourslondon@gmail.com';
+        const toAddress =  'info@m-rtours.co.uk';
 
         const mailOptions = {
             from: process.env.EMAIL_FROM || 'website@m-rtours.co.uk',
